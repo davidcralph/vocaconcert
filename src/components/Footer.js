cb29a7d6ed479f17fc4a929527aaf2430c3cebf1
David Ralph <me@davidcralph.co.uk>
2022-08-12 12:21:34 +0100

    fix bug in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <div class="container">
-      <hr class="col-xs-12" />
+    <div className="container">
+      <hr className="col-xs-12" />
       <footer className="footer">
         <p className="col-md-4 mb-0 text-muted">Copyright © David Ralph 2022</p>
       </footer>
